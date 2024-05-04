2f66403b5672845dee94118915aa41134e904de2
Brendan Blanchard <brendano257@gmail.com>
2024-05-04 07:38:32 -0400

    Cargo fmt

--- examples/live_query_orders_info.rs
+++ examples/live_query_orders_info.rs
@@ -1,7 +1,7 @@
 use kraken_async_rs::clients::core_kraken_client::CoreKrakenClient;
 use kraken_async_rs::clients::kraken_client::KrakenClient;
 use kraken_async_rs::crypto::nonce_provider::{IncreasingNonceProvider, NonceProvider};
-use kraken_async_rs::request_types::{OrderRequest};
+use kraken_async_rs::request_types::OrderRequest;
 use kraken_async_rs::secrets::secrets_provider::EnvSecretsProvider;
 use std::fs::File;
 use std::sync::Arc;
