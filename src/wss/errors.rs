53896e4827b495af94701e83c3523e27b60bddf8
Brendan Blanchard <brendano257@gmail.com>
2024-11-23 07:13:12 -0500

    Move error testing to WSSError file

--- src/wss/errors.rs
+++ src/wss/errors.rs
@@ -51,3 +51,28 @@ impl Error for WSSError {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::wss::errors::WSSError;
+    use serde::de::Error as DeError;
+    use serde_json::Error as SerdeError;
+    use tokio_tungstenite::tungstenite::Error as TungsteniteError;
+    use url::ParseError as UrlParseError;
+
+    #[test]
+    fn test_error_conversion() {
+        let serde_error = SerdeError::custom("id");
+        let tungstenite_error = TungsteniteError::ConnectionClosed;
+        let url_parse_error = UrlParseError::InvalidPort;
+
+        let error = WSSError::from(serde_error);
+        assert!(matches!(error, WSSError::Serde { .. }));
+
+        let error = WSSError::from(tungstenite_error);
+        assert!(matches!(error, WSSError::WSS { .. }));
+
+        let error = WSSError::from(url_parse_error);
+        assert!(matches!(error, WSSError::UrlParse { .. }));
+    }
+}
